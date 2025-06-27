653ff4ade24f411a791b1e22da1dda02dbd3a3b9
Owais-Siddique-11 <siddiqueowais499@gmail.com>
2025-06-27 16:49:58 +0530

    Added create Flight crud

---
+++ src/repository/flight-repository.js
@@ -0,0 +1,14 @@
+const {Flight}= require('../models/flights');
+
+class FlightRepository{
+    async createFlight(data){
+        try {
+            const flight = await Flights.create(data);
+            return flight;
+        } catch (error) {
+            console.log("something went wrong in repository layer");
+            throw {error}; 
+        }
+    }
+}
+module.exports = FlightRepository;
